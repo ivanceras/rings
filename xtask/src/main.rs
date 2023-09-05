3e294a4d794a3a2eb1033672eb2fc5e7b327871d
Jovansonlee Cesar <ivanceras@gmail.com>
2023-09-06 03:00:42 +0800

    add xtask

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+use xshell::{cmd, Shell};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// generate asset.rs file
+    GenerateAssets,
+    /// fix the files replacing kebab-case to snake_case
+    FixFilenames,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let sh = Shell::new()?;
+    let base_dir = "icons";
+    match args.command {
+        Commands::FixFilenames => {
+            let svg_files = sh.read_dir(base_dir)?;
+            for file in svg_files {
+                let ext = file.extension().expect("must have extension");
+                let mut path_buf = file.to_path_buf();
+                if ext == "svg" {
+                    let filename = file.file_name().expect("must have a filename");
+                    let filename = filename.to_str().expect("must be a valid filename");
+                    let new_filename = filename.replace("-", "_");
+                    path_buf.set_file_name(&new_filename);
+                    std::fs::rename(file, new_filename)?;
+                }
+            }
+        }
+        Commands::GenerateAssets => {
+            let svg_files = sh.read_dir(base_dir)?;
+            for file in svg_files {
+                println!("{:?} -> {:?}", file.file_name(), file.file_stem());
+            }
+        }
+    }
+    Ok(())
 }
